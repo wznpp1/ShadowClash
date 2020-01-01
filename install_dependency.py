10037cdead79851a55971addb3598296b108bb6a
wznpp1 <wznpp1@hotmail.com>
2020-01-01 09:45:39 +0800

    Update install_dependency.py

--- install_dependency.py
+++ install_dependency.py
@@ -8,34 +8,6 @@ if __name__ == "__main__":
         os.mkdir("framework")
     except:
         pass
-    print("[-] delete old files...")
-    if platform.system() == "Windows":
-        os.system("del /f /q .\\resources\\Country.mmdb")
-        shutil.rmtree("./resources/clashxdashboard")
-        shutil.rmtree("./resources/yacddashboard")
-        os.system("del /f /q GeoLite2-Country.*")
-    else:
-        os.system("rm -f ./resources/Country.mmdb")
-        os.system("rm -rf ./resources/clashxdashboard")
-        os.system("rm -rf ./resources/yacddashboard")
-        os.system("rm -f GeoLite2-Country.*")
-    print("[+] install mmdb...")
-    if platform.system() == "Windows":
-        os.system("certutil.exe -urlcache -split -f https://gitee.com/J92Z53/PlistServer/raw/master/GeoLite2-Country.tar.gz")
-        os.system("7z.exe x GeoLite2-Country.tar.gz")
-        os.system("7z.exe x GeoLite2-Country.tar")
-        for each in os.listdir("."):
-            if (each.startswith("GeoLite2-Country_")):
-                shutil.move(each + "/GeoLite2-Country.mmdb", "resources/Country.mmdb")
-        os.system("del /f /q GeoLite2-Country.tar.gz")
-        os.system("del /f /q GeoLite2-Country.tar")
-        os.system("del /f /q GeoLite2-Country_*")
-    else:
-        os.system("curl -O https://gitee.com/J92Z53/PlistServer/raw/master/GeoLite2-Country.tar.gz")
-        os.system("tar -zxvf GeoLite2-Country.tar.gz")
-        os.system("mv GeoLite2-Country_*/GeoLite2-Country.mmdb ./resources/Country.mmdb")
-        os.system("rm GeoLite2-Country.tar.gz")
-        os.system("rm -r GeoLite2-Country_*")
     os.chdir("resources")
     print("[+] install clashx dashboard...")
     os.system("git clone -b gh-pages https://github.com/Dreamacro/clash-dashboard.git clashxdashboard")
